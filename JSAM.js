c9a3babc7ef3c911d0b4635ec5eea8788d693e97
sarahwoolsey87 <shedontusejelly0@gmail.com>
2023-06-15 23:58:31 +0100

    Updated CSS styling and added boxes

--- JSAM.js
+++ JSAM.js
@@ -1,120 +1,107 @@
-
-
-
-//console.log ("testing");
-
-let refer;
-
-refer = prompt("Hey there, what's your name? If you don't tell me, I'm gonna call you 'null");
-alert("Welcome to my page " +refer+ " I hope you're having a great day!");
-
-let answer1 = confirm ("Press OK if you intended to visit my website?");
-//console.log(answer1);
-
-if (answer1) {
-//console.log("Awesome -it's great to have you here");
-alert("Awesome - it's great to have you here!");
-} else {
-//console.log("Why don't you stay anyway!");
-alert("Why don't you stay anyway?");
-}
-
+function greeting() {
+  let refer;
+  
+  refer = prompt("Hey there, what's your name? If you press 'cancel', I'm gonna call you 'null'");
+  alert("Welcome to my page " + refer + "! I hope you're having a great day!");
+  
+  let answer1 = confirm("Press OK if you intended to visit my website?");
+  
+  if (answer1) {
+    alert("Awesome - it's great to have you here!");
+  } else {
+    alert("Why don't you stay anyway?");
+  }
+  }
+  
+greeting();
+  
+function loner() {
 let friend = prompt ("Are we friends? Please answer yes/no or y/n only").toLowerCase();
-
+  
 while (friend != "yes" && friend != "no" && friend != "y" && friend != "n") {
-  alert("Please answer yes/no or y/n only");
-  friend = prompt("Are we friends? Please answer yes/no or y/n only").toLowerCase();
+alert("Please answer yes/no or y/n only");
+friend = prompt("Are we friends? Please answer yes/no or y/n only").toLowerCase();
 }
-
-if (friend === "yes" || friend === "y") {
-//console.log("Awesome - let's party like it's 1969!");
+  
+  if (friend === "yes" || friend === "y") {
 alert("Awesome - let's party like it's 1969");
-
-
 } else if (friend === "no" || friend === "n") {
-//console.log("Aw man, that sucks. We should change that");
 alert("Aw man, that sucks. We should change that.");
 }
+}
 
+loner();
+  
+function bullshit() {
 let language = prompt ("Do you believe that I can speak fluent Cantonese? Please answer yes/no or y/n only").toLowerCase();
-
+  
 while (language != "yes" && language != "no" && language != "y" && language != "n") {
-  alert("Please answer yes/no or y/n only");
-  language = prompt("Do you believe that I can speak fluent Cantonese?").toLowerCase();
+alert("Please answer yes/no or y/n only");
+language = prompt("Do you believe that I can speak fluent Cantonese?").toLowerCase();
 }
-
+  
 if (language === "yes" || language === "y") {
-//console.log("Wow - you think too highly of me. Have you seen how bad I am at pushing a simple Git?");
 alert("Wow - you think too highly of me. Have you seen how bad I am at pushing a simple Git?");
-
-} else if (language === "no" || language === "n") 
-//console.log("You're 100% correct although I'm a little offended.");
+} else if (language === "no" || language === "n") {
 alert("You're 100% correct although I'm a little offended.");
-
-let cheese = prompt ("Do you consume at least one type of cheese at least once per day? Please answer yes/no or y/n only").toLowerCase();
-
-while (cheese != "yes" && cheese != "no" && cheese != "y" && cheese != "n") {
-  alert("Please answer yes/no or y/n only");
-  cheese = prompt("Do you consume at least one type of cheese at least once per day? Please answer yes/no or y/n only").toLowerCase();
 }
-
+}
+  
+bullshit();
+  
+function cheeseLover() {
+let cheese = prompt ("Do you consume at least one type of cheese at least once per day? Please answer yes/no or y/n only").toLowerCase();
+  
+while (cheese != "yes" && cheese != "no" && cheese != "y" && cheese != "n") {
+alert("Please answer yes/no or y/n only");
+cheese = prompt("Do you consume at least one type of cheese at least once per day? Please answer yes/no or y/n only").toLowerCase();
+}
+  
 if (cheese === "yes" || cheese === "y") {
-//console.log("Awesome - let's party like it's 1969!");
 alert("Isn't it the best? I enjoy it at least once per hour.");
-
 } else if (cheese === "no" || cheese === "n") {
-//console.log("Aw man, that sucks. We should change that");
 alert("That's the saddest story I've cheddar heard.");
 }
-
-// Initial setup of variables.
+}
+  
+cheeseLover();
+  
 let lowerLimit = 1;
 let upperLimit = 25;
 let tries = 4;
 let answer = Math.floor(Math.random() * (upperLimit - lowerLimit + 1)) + lowerLimit;
 let guess = '';
 let message = 'Guess which number I\'m thinking of between ' + lowerLimit + ' and ' + upperLimit + ':';
-
-// Keep prompting the user for a guess until the game ends.
+  
 while (tries > 0) {
-  // Prompt the user for a guess.
-  guess = prompt(message, guess);
-
+guess = prompt(message, guess);
 
 if (guess === null) {
 alert('Okay then, spoilsport!');
 break;
-}
-
-else if (isFinite(guess) && guess !== '') {
+} else if (isFinite(guess) && guess !== '') {
 guess = +guess;
-
+  
 if (guess < lowerLimit) {
 alert('Can you not read? I said it shouldn\'t be lower than ' + lowerLimit + '.');
-}
-else if (guess > upperLimit) {
+} else if (guess > upperLimit) {
 alert('Can you not read? I said it can\'t be higher than ' + upperLimit + '.');
-}
-else if (guess > answer) {
+} else if (guess > answer) {
 alert('Nope - your guess is too high.');
-}
-else if (guess < answer) {
+} else if (guess < answer) {
 alert('Nope - your guess is too low.');
-}
-
-else {
+} else {
 alert('Awesome guess - gold star for you!');
 break;
 }
-}
-else {
+} else {
 alert('That is not a number, now is it?');
 }
 
 tries--;
 }
-
+  
 if (tries === 0) {
 alert('Sorry, you ran out of attempts. The correct answer was ' + answer + '.');
 }
-
+  
---
+++ README.md
@@ -0,0 +1,17 @@
+# Reading-Notes
+
+Tuesday: 
+I wrote the content in basic HTML and added some text boxes, along with a prompt, name input and alert. I also created a series of  yes/no (true/false) questions.
+
+Wednesday: I used a While loop for an random number (maths.random) game and created an array to ask the user to guess one of the coolest countries I'd been to (3 possible answers)
+
+Thursday: 
+Updated some styling on CSS. Chidi (Navigator) reviewed  problems with me and gave me advice on code to change/tidy up - applied changes those (landing page update, use of containers, CSS organisation etc). I also changed all the questions to functions (and invocations) as per the lab task. I added 'Flex' on Chidi's suggestion to position my social media buttons.
+I watched this video to help with my boxes (but it required a lot of repairs and experimentation with Flex!)
+
+Issues I'm having with the code:
+
+Why won't my Header center? I've tried so many things.
+
+Reminder on linking Google fonts?
+
